3647e055f4b8dab9e97942c82a92415329cdadd5
shamam <kafri.shamam@gmail.com>
2025-03-03 12:10:56 +0300

    Initial Commit

---
+++ AromicTests/AromicTests.swift
@@ -0,0 +1,17 @@
+//
+//  AromicTests.swift
+//  AromicTests
+//
+//  Created by Shamam Alkafri on 03/03/2025.
+//
+
+import Testing
+@testable import Aromic
+
+struct AromicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
